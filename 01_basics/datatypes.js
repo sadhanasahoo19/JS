be1fa1f57aa0ae4494f1721c608d4c8ad4b1c553
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-07-25 04:48:13 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+// "use strict"; //treat all js code as newer version
+// // alert(3+3) //we are using nodejs not browser
+// console.log(2+2)
+// console.log("sadhana")
+
+//number = 2 to the power 52
+//bigInt = wee dont use this much just only use for stockmarket, trading,
+//string="", ''
+//null == standalone value , it is the representation of empty value, the output of this will be as a "object"
+//boolean= true/false
+//undefined = when we dont defined the value to the variable output will be come as "undefined"
+//symbol= unique
+//object 
+
+console.log(typeof "pawar")
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accId = 12344
+let accName ="sadhana"
+var accPass = "21344"
+// we prefer not to user var because of issue in block scope and functional scope
+
+accCity = "jaipur"
+// accId = 2 not allowed const cannot be changed
+console.log(accId);
+accName = "chinu"
+accPass="212121"
+accCity="mumbai"
+console.table([accName, accPass, accCity]);
+
