92951dc9e7f89c205e1d9d6ee037676be6b7f545
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-07-25 16:30:25 +0000

    number and maths

---
+++ 01_basics/numaths.js
@@ -0,0 +1,30 @@
+// const score = 100
+// console.log(score);
+
+// const newScore = new Number(300)
+// console.log(newScore);
+// console.log(newScore.toString());
+// console.log(newScore.toString().length);
+// console.log(newScore.toFixed(2));
+// const number= 23.454
+// console.log(number.toPrecision(4));
+
+// const hundreds =1000
+// console.log(hundreds.toLocaleString());
+
+
+//+++++++++++++++++mathss+++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(42.3));
+console.log(Math.min(2,4,-4,2,1,7));
+console.log(Math.max(2,4,-4,2,1,7));
+console.log(Math.ceil(4.5));//round off to biggest value
+console.log(Math.floor(4.5));//round off to lowest value
+console.log(Math.random());//values always between 0-1
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max =20
+console.log(Math.floor(Math.random()*(max - min + 1))+ min);
