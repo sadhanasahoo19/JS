0bc0211fb3f09af14ebb8e6f05c6b699fe701015
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-07-25 06:09:37 +0000

    str to number

---
+++ 01_basics/stringtonumber.js
@@ -0,0 +1,23 @@
+// let value = 3
+// let negVal = -value
+// console.log(negVal)
+
+// console.log(2+2)
+
+let str1 = "hello"
+let str2 = " chinu"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" +2 +2) //if string is first then everything will be treat as the string
+console.log(1 + 2 +"2") //if number operation is first and last is string then it will not treat as string
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2= num3 =2+2
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
