8853bf5224d636bcbea4b7076031a10dfc352a66
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-07-25 05:48:24 +0000

    conversion of the datatype

---
+++ 01_basics/dtconversion.js
@@ -0,0 +1,19 @@
+let score = "true"
+console.log(typeof score)
+console.log(typeof (score)) //also write as  a method in "()"
+
+let valueInNum = Number(score)
+// console.log(typeof valueInNum)
+console.log(valueInNum)
+
+//"33" =33
+//"33abc"= NaN
+//"true"= 1; false=2
+
+let isLogIn = 1
+let booleanIsLogIn = Boolean(isLogIn)
+console.log(booleanIsLogIn) //1= true, 0= false, ""=false, "hello"=true
+
+ let sumNum = 33
+ let stringNum = String(sumNum)
+ console.log(stringNum)
