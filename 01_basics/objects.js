44a82969f16901187df7bf885adb4763c095512e
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-07-26 08:02:16 +0000

    objects of js

---
+++ 01_basics/objects.js
@@ -0,0 +1,14 @@
+//obejcts literals= stored in key-value pair
+
+const sym = Symbol("keys1")//how to declared a symbol as a key
+
+const jsUser={
+    name:"chinu",
+    age:18,
+    location: "mumbai",
+    email:"chinu@.com",
+    isLoggedIn: false,
+    
+}
+console.log(jsUser.email);
+console.log(jsUser["email"]);//also access the objects in this way
