8dd7c3c58051c1cf72eb4a54e9df830a51427be1
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-07-27 03:45:36 +0000

    objects in js

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -5,6 +5,7 @@ const sym = Symbol("keys1")//how to declared a symbol as a key
 const jsUser={
     name:"chinu",
     age:18,
+    [sym] : "keys1",
     location: "mumbai",
     email:"chinu@.com",
     isLoggedIn: false,
@@ -12,3 +13,17 @@ const jsUser={
 }
 console.log(jsUser.email);
 console.log(jsUser["email"]);//also access the objects in this way
+console.log(jsUser[sym]);
+
+jsUser.email = "spmailcom"
+// Object.freeze(jsUser) //freeze: to lock the value so that no one change it again
+console.log(jsUser);
+
+jsUser.greetings = function(){
+    console.log("hello world");
+}
+jsUser.greetingsTwo = function(){
+    console.log(`hello world ${this.name}`);//jab bhi hume same object ko reference krna hai then use "this"
+}
+console.log(jsUser.greetings());
+console.log(jsUser.greetingsTwo());
