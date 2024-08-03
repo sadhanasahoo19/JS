0f790f220239610d6e4cf203463954a2fc25b360
sadhanasahoo19 <146162958+sadhanasahoo19@users.noreply.github.com>
2024-08-03 15:07:53 +0000

    foor,while, do while loops in js

---
+++ 01_basics/forloop.js
@@ -0,0 +1,79 @@
+//for loop
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+// console.log(element);//not access outside the scope
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if(element == 5){
+//         console.log("5 is equal to element");
+        
+//     }
+//     console.log(element);
+    
+// }
+
+//print the table
+// for (let i = 1; i <=10; i++) {
+//     for (let j = 0; j <=10; j++) {
+//         console.log(i*j);
+//     }
+// }
+
+// arr=["tina ","rina","om"]
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     console.log(element);
+    
+// }
+
+//break continue
+// for (let index = 1; index <=20; index++) {
+//     if(index==5){
+//         console.log("5 is detected");
+//         break
+        
+//     }
+//     console.log(`value of i is ${index}`);
+
+// }
+// for (let index = 1; index <=20; index++) {
+//     if(index==5){
+//         console.log("5 is detected");
+//         continue
+        
+//     }
+//     console.log(`value of i is ${index}`);
+// }
+
+
+//while loop
+// let i =0
+// while (i<11) {
+//     console.log(`value of i is ${i}`);
+//     i=i+2
+// }
+
+
+// let myarr= ["tina ","rina","om"]
+// let arr=0
+// while (arr<myarr.length) {
+//     console.log(`value of arr is: ${myarr[arr]}`);
+//     arr++    
+// }
+
+
+//==========do while loop==========
+// do {//kaam pehle badme condition check krte he
+    
+// } while (condition);
+
+
+let score= 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
